a747e5eaa702fd0510bd2acd120eef29ddba9a4b
philipp-hethey <philipp@philipp-hethey.de>
2024-10-23 12:53:53 +0200

    Another little Updates (-;

--- language_manager.py
+++ language_manager.py
@@ -45,34 +45,20 @@ class LanguageManager:
 
         if language in self.languages:
             if key not in self.languages[language]:
+                # Always add missing keys to the default language file at the end
                 self.languages[self.default_language][key] = fallback_message
-                self.update_language_file(self.default_language, key, fallback_message)
+                self.append_to_language_file(self.default_language, key, fallback_message)
             return self.languages[language].get(key, fallback_message)
         else:
             return self.languages[self.default_language].get(key, fallback_message)
 
-    def update_language_file(self, language_code, key, message):
-        """Update an existing language file with new keys."""
+    def append_to_language_file(self, language_code, key, message):
+        """Append a new key to the existing language file."""
         file_path = f'{self.language_dir}/{language_code}.yml'
         if os.path.exists(file_path):
-            with open(file_path, 'r', encoding='utf-8') as file:
-                lang_data = yaml.safe_load(file)
-                lang_data[key] = message
-
-            with open(file_path, 'w', encoding='utf-8') as file:
-                yaml.dump(lang_data, file, allow_unicode=True)
+            with open(file_path, 'a', encoding='utf-8') as file:  # 'a' mode appends to the file
+                yaml.dump({key: message}, file, allow_unicode=True)
 
     def set_language(self, language_code):
         """Change the current language."""
         self.current_language = language_code
-
-
-# Example usage
-language_manager = LanguageManager(default_language='de_DE')
-
-print(language_manager.get_message('welcome.message', 'Hallo, willkommen in diesem Programm!'))
-
-language_manager.set_language('en_US')
-print(language_manager.get_message('welcome.message', 'Welcome to this program!'))
-
-print(language_manager.get_message('welcome.message', 'Hallo, willkommen in diesem Programm!', 'de_DE'))
