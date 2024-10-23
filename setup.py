9b043cc977c27011952d3733f62951e2156ed67d
philipp-hethey <philipp@philipp-hethey.de>
2024-10-23 12:54:20 +0200

    Version Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='multi_language_framework',
-    version='1.0',
+    version='1.1',
     py_modules=['language_manager'],
     install_requires=['pyyaml'],
     include_package_data=True,
